ec50a66c225371552110ba1d195d0a5f1d806605
Josh Kim <hyorkim11@gmail.com>
2015-08-07 14:05:44 -0400

    8/7/15 - 2:05PM

--- app/src/main/java/empaticae4/hrker/com/empaticae4/AssessmentActivity.java
+++ app/src/main/java/empaticae4/hrker/com/empaticae4/AssessmentActivity.java
@@ -1,16 +1,14 @@
 package empaticae4.hrker.com.empaticae4;
 
-
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class Notification extends Activity  {
+public class AssessmentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
-
+        setContentView(R.layout.activity_assessment);
     }
 
 }
--- app/src/main/java/empaticae4/hrker/com/empaticae4/MainActivity.java
+++ app/src/main/java/empaticae4/hrker/com/empaticae4/MainActivity.java
@@ -1,10 +1,12 @@
 package empaticae4.hrker.com.empaticae4;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -23,11 +25,11 @@ import com.empatica.empalink.config.EmpaStatus;
 import com.empatica.empalink.delegate.EmpaDataDelegate;
 import com.empatica.empalink.delegate.EmpaStatusDelegate;
 
+import java.util.Date;
+
 
 public class MainActivity extends AppCompatActivity implements EmpaDataDelegate, EmpaStatusDelegate {
 
-    NotificationManager notiManager = (NotificationManager)
-            getSystemService(NOTIFICATION_SERVICE);
 
     private static final int REQUEST_ENABLE_BT = 1;
     private static final long STREAMING_TIME = 10000;
@@ -52,8 +54,13 @@ public class MainActivity extends AppCompatActivity implements EmpaDataDelegate,
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialSetup();
+    }
+
+    private void initialSetup() {
 
         // Initialize variables that reference UI components
         statusLabel = (TextView) findViewById(R.id.status);
@@ -68,14 +75,10 @@ public class MainActivity extends AppCompatActivity implements EmpaDataDelegate,
         batteryLabel = (TextView) findViewById(R.id.battery);
         deviceNameLabel = (TextView) findViewById(R.id.deviceName);
 
-        // Create a new EmpaDeviceManager. MainActivity is both its data and status delegate.
+        // Create a new Empatica DeviceManager. MainActivity is both its data and status delegate.
         deviceManager = new EmpaDeviceManager(getApplicationContext(), this, this);
         // Initialize the Device Manager using your API key. You need to have Internet access at this point.
         deviceManager.authenticateWithAPIKey(EMPATICA_API_KEY);
-
-
-
-
 
     }
 
@@ -182,6 +185,7 @@ public class MainActivity extends AppCompatActivity implements EmpaDataDelegate,
 
     @Override
     public void didReceiveAcceleration(int x, int y, int z, double timestamp) {
+
         updateLabel(accel_xLabel, "" + x);
         updateLabel(accel_yLabel, "" + y);
         updateLabel(accel_zLabel, "" + z);
@@ -189,31 +193,38 @@ public class MainActivity extends AppCompatActivity implements EmpaDataDelegate,
 
     @Override
     public void didReceiveBVP(float bvp, double timestamp) {
+
         updateLabel(bvpLabel, "" + bvp);
     }
 
     @Override
     public void didReceiveBatteryLevel(float battery, double timestamp) {
+
         updateLabel(batteryLabel, String.format("%.0f %%", battery * 100));
     }
 
+    // GSR = EDA level
     @Override
     public void didReceiveGSR(float gsr, double timestamp) {
+
         updateLabel(edaLabel, "" + gsr);
     }
 
     @Override
     public void didReceiveIBI(float ibi, double timestamp) {
+
         updateLabel(ibiLabel, "" + ibi);
     }
 
     @Override
     public void didReceiveTemperature(float temp, double timestamp) {
+        
         updateLabel(temperatureLabel, "" + temp);
     }
 
     // Update a label with some text, making sure this is run in the UI thread
     private void updateLabel(final TextView label, final String text) {
+
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -225,18 +236,31 @@ public class MainActivity extends AppCompatActivity implements EmpaDataDelegate,
     // Notifications
     public void sendNoti(View v) {
 
-        Intent intent = new Intent(this, Notification.class);
-        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_drawer)
+                        .setContentTitle("Nudge from Your Empatica")
+                        .setContentText("Assessment is recommended")
+                        .addAction(R.drawable.ic_drawer, "No", null)
+                        .addAction(R.drawable.ic_drawer, "Sure", null);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setContentTitle("Notification from RiskWatch");
-        builder.setContentText("Please click to continue");
-        builder.setSmallIcon(R.drawable.ic_drawer);
-        builder.setContentIntent(pIntent);
-        builder.setAutoCancel(true);
-        builder.addAction(R.drawable.ic_drawer, "Call", pIntent);
-        builder.addAction(R.drawable.ic_drawer, "More", pIntent);
-        builder.addAction(R.drawable.ic_drawer, "And more", pIntent).build();
+        Intent notificationIntent = new Intent(this, AssessmentActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Create semi-unique notification ID
+        long time = new Date().getTime();
+        String tempStr = String.valueOf(time);
+        String last4Str = tempStr.substring(tempStr.length() - 5);
+        int notificationID = Integer.valueOf(last4Str);
+
+        // Push notification
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // hide the notification after its selected
+        builder.flags |= NotificationCompat.FLAG_AUTO_CANCEL;
+
+        manager.notify(notificationID, builder.build());
 
     }
 }
