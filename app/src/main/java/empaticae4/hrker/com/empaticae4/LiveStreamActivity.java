642350eda1219bc38cb641fce554f8b9970fc512
Josh <josh@friendslearn.com>
2015-08-11 21:14:47 -0400

    8/11/15 - 9:14PM

---
+++ app/src/main/java/empaticae4/hrker/com/empaticae4/LiveStreamActivity.java
@@ -0,0 +1,244 @@
+package empaticae4.hrker.com.empaticae4;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.empatica.empalink.ConnectionNotAllowedException;
+import com.empatica.empalink.EmpaDeviceManager;
+import com.empatica.empalink.config.EmpaSensorStatus;
+import com.empatica.empalink.config.EmpaSensorType;
+import com.empatica.empalink.config.EmpaStatus;
+import com.empatica.empalink.delegate.EmpaDataDelegate;
+import com.empatica.empalink.delegate.EmpaStatusDelegate;
+
+public class LiveStreamActivity extends AppCompatActivity implements EmpaDataDelegate, EmpaStatusDelegate {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final long STREAMING_TIME = 10000;
+    // Stops streaming 10 seconds after connection
+
+    private EmpaDeviceManager deviceManager;
+
+    private TextView accel_xLabel;
+    private TextView accel_yLabel;
+    private TextView accel_zLabel;
+    private TextView bvpLabel;
+    private TextView edaLabel;
+    private TextView ibiLabel;
+    private TextView temperatureLabel;
+    private TextView batteryLabel;
+    private TextView statusLabel;
+    private TextView deviceNameLabel;
+    private RelativeLayout dataCnt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_stream);
+        initialSetup();
+    }
+
+    private void initialSetup() {
+
+        // Initialize variables that reference UI components
+        statusLabel = (TextView) findViewById(R.id.status);
+        dataCnt = (RelativeLayout) findViewById(R.id.dataArea);
+        accel_xLabel = (TextView) findViewById(R.id.accel_x);
+        accel_yLabel = (TextView) findViewById(R.id.accel_y);
+        accel_zLabel = (TextView) findViewById(R.id.accel_z);
+        bvpLabel = (TextView) findViewById(R.id.bvp);
+        edaLabel = (TextView) findViewById(R.id.eda);
+        ibiLabel = (TextView) findViewById(R.id.ibi);
+        temperatureLabel = (TextView) findViewById(R.id.temperature);
+        batteryLabel = (TextView) findViewById(R.id.battery);
+        deviceNameLabel = (TextView) findViewById(R.id.deviceName);
+
+        // Create a new Empatica DeviceManager. MainActivity is both its data and status delegate.
+        deviceManager = new EmpaDeviceManager(getApplicationContext(), this, this);
+        // Initialize the Device Manager using your API key. You need to have Internet access at this point.
+        deviceManager.authenticateWithAPIKey("6c8d1b1459ff473fbc6e71d6ae76aa19");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        deviceManager.stopScanning();
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        // export captured data into a csv file before destroying cache
+
+        super.onDestroy();
+        deviceManager.cleanUp();
+
+    }
+
+    @Override
+    public void didDiscoverDevice(BluetoothDevice bluetoothDevice,
+                                  String deviceName, int rssi, boolean allowed) {
+
+        if (allowed) {
+            // Stop scanning. The first allowed device will do.
+            deviceManager.stopScanning();
+            try {
+                // Connect to the device
+                deviceManager.connectDevice(bluetoothDevice);
+                updateLabel(deviceNameLabel, "To: " + deviceName);
+            } catch (ConnectionNotAllowedException e) {
+                // This should happen only if you try to connect when allowed == false.
+                Toast.makeText(LiveStreamActivity.this, "Sorry, there was an " +
+                        "error connecting to this device", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void didRequestEnableBluetooth() {
+
+        // Request the user to enable Bluetooth
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+
+            // The user chose not to enable Bluetooth
+            Toast.makeText(LiveStreamActivity.this, "Please enable bluetooth", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void didUpdateSensorStatus(EmpaSensorStatus status, EmpaSensorType type) {
+        // No need to implement this right now
+    }
+
+    @Override
+    public void didUpdateStatus(EmpaStatus status) {
+
+        // Update the UI
+        updateLabel(statusLabel, status.name());
+
+        // The device manager is ready for use
+        if (status == EmpaStatus.READY) {
+
+            updateLabel(statusLabel, status.name() + " - Turn on your device");
+            // Start scanning
+            deviceManager.startScanning();
+            // The device manager has established a connection
+        } else if (status == EmpaStatus.CONNECTED) {
+
+            // Stop streaming after STREAMING_TIME
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    dataCnt.setVisibility(View.VISIBLE);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Disconnect device
+                            deviceManager.disconnect();
+                        }
+                    }, STREAMING_TIME);
+
+                }
+            });
+            // The device manager disconnected from a device
+        } else if (status == EmpaStatus.DISCONNECTED) {
+
+            updateLabel(deviceNameLabel, "");
+        }
+    }
+
+    @Override
+    public void didReceiveAcceleration(int x, int y, int z, double timestamp) {
+
+        updateLabel(accel_xLabel, "" + x);
+        updateLabel(accel_yLabel, "" + y);
+        updateLabel(accel_zLabel, "" + z);
+    }
+
+    @Override
+    public void didReceiveBVP(float bvp, double timestamp) {
+
+        updateLabel(bvpLabel, "" + bvp);
+    }
+
+    @Override
+    public void didReceiveBatteryLevel(float battery, double timestamp) {
+
+        updateLabel(batteryLabel, String.format("%.0f %%", battery * 100));
+    }
+
+    // GSR = EDA level
+    @Override
+    public void didReceiveGSR(float gsr, double timestamp) {
+
+        updateLabel(edaLabel, "" + gsr);
+    }
+
+    @Override
+    public void didReceiveIBI(float ibi, double timestamp) {
+
+        updateLabel(ibiLabel, "" + ibi);
+    }
+
+    @Override
+    public void didReceiveTemperature(float temp, double timestamp) {
+
+        updateLabel(temperatureLabel, "" + temp);
+    }
+
+    // Update a label with some text, making sure this is run in the UI thread
+    private void updateLabel(final TextView label, final String text) {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                label.setText(text);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_live_stream, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/empaticae4/hrker/com/empaticae4/MainActivity.java
+++ app/src/main/java/empaticae4/hrker/com/empaticae4/MainActivity.java
@@ -1,36 +1,24 @@
 package empaticae4.hrker.com.empaticae4;
 
-import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.empatica.empalink.ConnectionNotAllowedException;
-import com.empatica.empalink.EmpaDeviceManager;
-import com.empatica.empalink.config.EmpaSensorStatus;
-import com.empatica.empalink.config.EmpaSensorType;
-import com.empatica.empalink.config.EmpaStatus;
-import com.empatica.empalink.delegate.EmpaDataDelegate;
-import com.empatica.empalink.delegate.EmpaStatusDelegate;
 import com.yalantis.contextmenu.lib.ContextMenuDialogFragment;
 import com.yalantis.contextmenu.lib.MenuObject;
 import com.yalantis.contextmenu.lib.MenuParams;
@@ -42,39 +30,18 @@ import java.util.Date;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements EmpaDataDelegate, EmpaStatusDelegate, OnMenuItemClickListener, OnMenuItemLongClickListener {
+public class MainActivity extends ActionBarActivity implements OnMenuItemClickListener,
+        OnMenuItemLongClickListener {
 
 
-    private static final int REQUEST_ENABLE_BT = 1;
-    private static final long STREAMING_TIME = 10000;
-    // Stops streaming 10 seconds after connection
-    // insert your API Key here
-
-    private EmpaDeviceManager deviceManager;
-
-    private TextView accel_xLabel;
-    private TextView accel_yLabel;
-    private TextView accel_zLabel;
-    private TextView bvpLabel;
-    private TextView edaLabel;
-    private TextView ibiLabel;
-    private TextView temperatureLabel;
-    private TextView batteryLabel;
-    private TextView statusLabel;
-    private TextView deviceNameLabel;
-    private RelativeLayout dataCnt;
-
-    private MenuObject item1;
     private FragmentManager fragmentManager;
     private DialogFragment mMenuDialogFragment;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initialSetup();
 
         fragmentManager = getSupportFragmentManager();
         initToolbar();
@@ -83,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements EmpaDataDelegate,
     }
 
     private void initMenuFragment() {
+
         MenuParams menuParams = new MenuParams();
         menuParams.setActionBarSize((int) getResources().getDimension(R.dimen.tool_bar_height));
         menuParams.setMenuObjects(getMenuObjects());
@@ -90,40 +58,23 @@ public class MainActivity extends AppCompatActivity implements EmpaDataDelegate,
         mMenuDialogFragment = ContextMenuDialogFragment.newInstance(menuParams);
     }
 
-    private void initialSetup() {
-
-        // Initialize variables that reference UI components
-        statusLabel = (TextView) findViewById(R.id.status);
-        dataCnt = (RelativeLayout) findViewById(R.id.dataArea);
-        accel_xLabel = (TextView) findViewById(R.id.accel_x);
-        accel_yLabel = (TextView) findViewById(R.id.accel_y);
-        accel_zLabel = (TextView) findViewById(R.id.accel_z);
-        bvpLabel = (TextView) findViewById(R.id.bvp);
-        edaLabel = (TextView) findViewById(R.id.eda);
-        ibiLabel = (TextView) findViewById(R.id.ibi);
-        temperatureLabel = (TextView) findViewById(R.id.temperature);
-        batteryLabel = (TextView) findViewById(R.id.battery);
-        deviceNameLabel = (TextView) findViewById(R.id.deviceName);
-
-        // Create a new Empatica DeviceManager. MainActivity is both its data and status delegate.
-        deviceManager = new EmpaDeviceManager(getApplicationContext(), this, this);
-        // Initialize the Device Manager using your API key. You need to have Internet access at this point.
-        deviceManager.authenticateWithAPIKey("6c8d1b1459ff473fbc6e71d6ae76aa19");
-
-    }
-
     private List<MenuObject> getMenuObjects() {
 
         List<MenuObject> menuObjects = new ArrayList<>();
 
         MenuObject close = new MenuObject();
-        close.setResource(R.drawable.ic_drawer);
+        close.setResource(R.mipmap.ic_close);
+
+        MenuObject send = new MenuObject("Send message");
+        send.setResource(R.mipmap.ic_launcher);
 
         menuObjects.add(close);
+        menuObjects.add(send);
         return menuObjects;
     }
 
     private void initToolbar() {
+
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         TextView mToolBarTextView = (TextView) findViewById(R.id.text_view_toolbar_title);
         setSupportActionBar(mToolbar);
@@ -151,156 +102,6 @@ public class MainActivity extends AppCompatActivity implements EmpaDataDelegate,
                 transaction.addToBackStack(backStackName);
             transaction.commit();
         }
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        deviceManager.stopScanning();
-    }
-
-    @Override
-    protected void onDestroy() {
-
-        // export captured data into a csv file before destroying cache
-
-        super.onDestroy();
-        deviceManager.cleanUp();
-
-    }
-
-
-    @Override
-    public void didDiscoverDevice(BluetoothDevice bluetoothDevice,
-                                  String deviceName, int rssi, boolean allowed) {
-
-        if (allowed) {
-            // Stop scanning. The first allowed device will do.
-            deviceManager.stopScanning();
-            try {
-                // Connect to the device
-                deviceManager.connectDevice(bluetoothDevice);
-                updateLabel(deviceNameLabel, "To: " + deviceName);
-            } catch (ConnectionNotAllowedException e) {
-                // This should happen only if you try to connect when allowed == false.
-                Toast.makeText(MainActivity.this, "Sorry, there was an " +
-                        "error connecting to this device", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    @Override
-    public void didRequestEnableBluetooth() {
-
-        // Request the user to enable Bluetooth
-        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
-
-            // The user chose not to enable Bluetooth
-            Toast.makeText(MainActivity.this, "Please enable bluetooth", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public void didUpdateSensorStatus(EmpaSensorStatus status, EmpaSensorType type) {
-        // No need to implement this right now
-    }
-
-    @Override
-    public void didUpdateStatus(EmpaStatus status) {
-
-        // Update the UI
-        updateLabel(statusLabel, status.name());
-
-        // The device manager is ready for use
-        if (status == EmpaStatus.READY) {
-
-            updateLabel(statusLabel, status.name() + " - Turn on your device");
-            // Start scanning
-            deviceManager.startScanning();
-            // The device manager has established a connection
-        } else if (status == EmpaStatus.CONNECTED) {
-
-            // Stop streaming after STREAMING_TIME
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-
-                    dataCnt.setVisibility(View.VISIBLE);
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            // Disconnect device
-                            deviceManager.disconnect();
-                        }
-                    }, STREAMING_TIME);
-
-                }
-            });
-            // The device manager disconnected from a device
-        } else if (status == EmpaStatus.DISCONNECTED) {
-
-            updateLabel(deviceNameLabel, "");
-        }
-    }
-
-    @Override
-    public void didReceiveAcceleration(int x, int y, int z, double timestamp) {
-
-        updateLabel(accel_xLabel, "" + x);
-        updateLabel(accel_yLabel, "" + y);
-        updateLabel(accel_zLabel, "" + z);
-    }
-
-    @Override
-    public void didReceiveBVP(float bvp, double timestamp) {
-
-        updateLabel(bvpLabel, "" + bvp);
-    }
-
-    @Override
-    public void didReceiveBatteryLevel(float battery, double timestamp) {
-
-        updateLabel(batteryLabel, String.format("%.0f %%", battery * 100));
-    }
-
-    // GSR = EDA level
-    @Override
-    public void didReceiveGSR(float gsr, double timestamp) {
-
-        updateLabel(edaLabel, "" + gsr);
-    }
-
-    @Override
-    public void didReceiveIBI(float ibi, double timestamp) {
-
-        updateLabel(ibiLabel, "" + ibi);
-    }
-
-    @Override
-    public void didReceiveTemperature(float temp, double timestamp) {
-
-        updateLabel(temperatureLabel, "" + temp);
-    }
-
-    // Update a label with some text, making sure this is run in the UI thread
-    private void updateLabel(final TextView label, final String text) {
-
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                label.setText(text);
-            }
-        });
     }
 
     // Notifications
