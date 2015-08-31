fd1a1561653f3fdce43d7adf37fd8b93d2375126
Josh Kim <hyorkim11@gmail.com>
2015-08-31 19:49:31 -0400

    8/31/15 - 7:49PM

--- app/src/main/java/empaticae4/hrker/com/empaticae4/Reports/NegativeActivity2.java
+++ app/src/main/java/empaticae4/hrker/com/empaticae4/Reports/NegativeActivity2.java
@@ -2,6 +2,7 @@ package empaticae4.hrker.com.empaticae4.Reports;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,19 +12,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
 
 import java.util.Objects;
 
+import empaticae4.hrker.com.empaticae4.MainActivity;
 import empaticae4.hrker.com.empaticae4.R;
 
+// Cool thoughts selection upon what is going on choice
 public class NegativeActivity2 extends AppCompatActivity {
 
     public static final String DATAFILE = "userData";
     SharedPreferences sharedP = null;
 
-    RadioButton mOther;
+    RadioButton bOther;
     BootstrapButton bCancel, bContinue;
 
     @Override
@@ -36,16 +40,30 @@ public class NegativeActivity2 extends AppCompatActivity {
     void init() {
 
         sharedP = getSharedPreferences(DATAFILE, MODE_MULTI_PROCESS);
-        String tempString = sharedP.getString("custom_coolthought", "Other");
+        final String tempString = sharedP.getString("custom_coolthought", "Other");
 
         bCancel = (BootstrapButton)findViewById(R.id.bCancel);
+        bCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCancelAlert();
+            }
+        });
         bContinue = (BootstrapButton)findViewById(R.id.bContinue);
-        RadioButton bOther = (RadioButton) findViewById(R.id.bOther);
+        bContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openQuestionAlert();
+            }
+        });
+        bOther = (RadioButton) findViewById(R.id.bOther);
         bOther.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                openCustom();
+                if (Objects.equals(tempString, "Other")) {
+                    openCustom();
+                }
             }
         });
 
@@ -97,7 +115,7 @@ public class NegativeActivity2 extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int id) {
 
                 String tempString = editor.getText().toString();
-                mOther.setText(tempString);
+                bOther.setText(tempString);
                 spEditor.putString("custom_coolthought", tempString).apply();
 
                 dialog.cancel();
@@ -115,6 +133,64 @@ public class NegativeActivity2 extends AppCompatActivity {
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
 
+    }
+
+    private void openCancelAlert() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(NegativeActivity2.this);
+        alertDialogBuilder.setTitle("");
+        alertDialogBuilder.setMessage("Are you sure you want to quit?");
+
+        // set positive button: Yes
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int id) {
+
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                Toast.makeText(getApplicationContext(), "You have quit your report", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // set negative button: No
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void openQuestionAlert() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(NegativeActivity2.this);
+        alertDialogBuilder.setTitle("");
+        alertDialogBuilder.setMessage("Planning on drinking?");
+
+        // set positive button: Yes
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int id) {
+
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // set negative button: No
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 
     @Override
